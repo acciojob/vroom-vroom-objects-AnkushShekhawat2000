08723c37ad7c5380e65b35f5d55c3249a4a09076
ANKUSH SHEKHAWAT <114976201+AnkushShekhawat2000@users.noreply.github.com>
2024-01-01 19:31:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-// Complete the js code
 function Car(make, model) {
-	this.make = make;
-	this.model = model;
+	this.make=make;
+	this.model=model;
 }
-
-Car.prototype.getMakeModel = function(){
-	return "${this.make} ${this.model}";
+Car.prototype.getMakeModel= function(){
+	return ${this.make} ${this.model};
 }
 
 function SportsCar(make, model, topSpeed) {
-	Car.call(this,make,model);
-	this.topSpeed = topSpeed;
+	this.make= make;
+	this.model= model;
+	this.topSpeed= topSpeed;
 }
-
-// Inherit the Car prototype
-SportsCar.prototype = Object.create(Car.prototype);
-
-// Add a method to the SportsCar prototype
-SportsCar.prototype.getTopSpeed = function () {
-    return this.topSpeed;
-};
-
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+SportsCar.prototype._proto_= Car.prototype;
+SportsCar.prototype.getTopSpeed= function(){
+	return this.topSpeed;
+}
